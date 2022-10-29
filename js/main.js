adad93025167e177c102815419166385f6951387
MaxGaynov <makcs17@mail.ru>
2022-10-29 18:41:34 +0300

    задание 4 готов

--- js/main.js
+++ js/main.js
@@ -11,10 +11,10 @@ const showRandInteger = (min, max) => {
   return Math.floor(rand);
 };
 
-const showRandIntegerWithDePo = (min, max, digitsADP) => {
+const showRandIntegerWithDePo = (min, max, digitsADP = 1) => {
   let rand;
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
+  const lower = Math.min(min, max);
+  const upper = Math.max(min, max);
   if (min > 0 && max > 0) {
     rand = lower - 0.5 + Math.random() * (upper - lower + 1);
   }
@@ -34,60 +34,70 @@ const timeOfCheck = ['12:00', '13:00', '14:00'];
 
 const createAvatarIndex = function () {
   if (showRandInteger(1, 10) < 10){
-    return `img/avatars/user0${showRandInteger(1, 9)}.png`;
+    return `img/avatars/user0${showRandInteger(2, 9)}.png`;
   }
   return 'img/avatars/user10.png';
 };
 
-const FEATURES_ARRAY_RANDOM_LENGTH = showRandInteger(1, 6);
-const varFeatures = new Set('wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner');
+const getArray = (arrayName) => {
+  const maxLength = arrayName.length;
+  const lengthOfArray = getRandomNumber(1, maxLength);
+  const array = [];
 
-const PHOTOS_ARRAY_RANDOM_LENGTH = showRandInteger(1, 3);
-const varPhotos = new Set('https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  while (array.length < lengthOfArray) {
+    const indexOfEl = getRandomNumber(0, maxLength - 1);
+    const el = arrayName[indexOfEl];
+
+    if (!array.includes(el)) {
+      array.push(el);
+    }
+  }
+  return array;
+
+  function getRandomNumber(from, to) {
+    return Math.floor(Math.random() * (to - from + 1)) + from;
+  }
+};
+
+const varFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const varPhotos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg');
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
+const addressIndex = `${showRandIntegerWithDePo(35.65000, 35.70000, 5)}, ${ showRandIntegerWithDePo(139.70000, 139.80000, 5)}`;
 
-const addressIndex = `${location[lat]}, ${location[lng]}`;
+for (let i = 0; i < 10; i++) {
+  const createObject = () => {
+    const author = {
+      avatar: createAvatarIndex()
+    };
 
-const randomFeatureIndex = Array.from({length: FEATURES_ARRAY_RANDOM_LENGTH}, varFeatures);
+    const offer = {
+      title: 'Сдам помещение',
+      address: addressIndex,
+      price: showRandInteger(1, 100),
+      type: showArrayRand(typeOfOffer),
+      rooms: showRandInteger(1, 100),
+      guests: showRandInteger(1, 100),
+      checkin: showArrayRand(timeOfCheck),
+      checkout: showArrayRand(timeOfCheck),
+      features: getArray(varFeatures),
+      description: 'Ухоженное помещение, сдается на долгий срок',
+      photos: getArray(varPhotos),
+    };
 
-const randomPhotosIndex = Array.from({length: PHOTOS_ARRAY_RANDOM_LENGTH}, varPhotos);
+    const location = {
+      lat: showRandIntegerWithDePo(35.65000, 35.70000, 5),
+      lng: showRandIntegerWithDePo(139.70000, 139.80000, 5),
+    };
 
-const author = {
-  avatar: createAvatarIndex()
-};
+    return {
+      author,
+      offer,
+      location,
+    };
 
-const offer = {
-  title: 'Сдам помещение',
-  address: addressIndex,
-  price: showRandInteger(),
-  type: showArrayRand(typeOfOffer),
-  rooms: showRandInteger(),
-  guests: showRandInteger(),
-  checkin: showArrayRand(timeOfCheck),
-  checkout: showArrayRand(timeOfCheck),
-  features: randomFeatureIndex,
-  description: 'Ухоженное помещение, сдается на долгий срок',
-  photos: randomPhotosIndex,
-};
-
-const location = {
-  lat: showRandIntegerWithDePo(35.65000, 35.70000, 5),
-  lng: showRandIntegerWithDePo(139.70000, 139.80000, 5),
-};
-
-const createObject = () => {
-  const authorIndex = author[avatar];
-  const offerIndex = `${offer[title]}, ${offer[address]}, ${offer[price]}, ${offer[type]}, ${offer[rooms]}, ${offer[guests]}, ${offer[checkin]}, ${offer[checkout]}, ${offer[features]}, ${offer[description]}, ${offer[photos]}`;
-  const locationIndex = `${location[lat]}, ${location[lng]}`;
-
-  return {
-    author: authorIndex,
-    offer: offerIndex,
-    location: locationIndex,
   };
-};
-
-// eslint-disable-next-line no-console
-console.log(createObject());
+  // eslint-disable-next-line no-console
+  console.log(createObject());
+}
